be6537924598868a9421a63f55f39529439c5cf5
joao-pedro-de-moura <jpmouraj@gmail.com>
2023-01-19 10:56:17 -0300

    Inicializando projeto para avaliação back-end attornatus

---
+++ src/main/java/Attornatus/evaluation/EvaluationAttornatusApplication.java
@@ -0,0 +1,13 @@
+package Attornatus.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationAttornatusApplication.class, args);
+	}
+
+}
